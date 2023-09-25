3d0548b414bf0abb8cf9a5410686fba450468409
zlx7799 <45277683+zlx7799@users.noreply.github.com>
2023-09-25 22:45:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DisplayResult from './components/DisplayResult/Index.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+      <DisplayResult/>
+    </div>
+</template>
+
+<style scoped>
+.wrapper{
+  width: 1000px;
+  height: 500px;
+  margin: auto;
+}
+</style>
---
+++ src/components/DisplayResult/Index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { onMounted, ref, toValue } from "vue";
+type DOMRef = HTMLElement | null
+
+const displayContainer = ref<DOMRef>(null)
+/**
+ * @description: 获取元素宽高
+ * @param {*} el
+ * @return {*}
+ */
+const getElSize = (el: DOMRef) => {
+  return {
+    width: el?.clientWidth,
+    height: el?.clientHeight,
+  }
+}
+
+onMounted(() => {
+  getElSize(toValue(displayContainer))
+})
+
+</script>
+
+<template>
+  <div class="display-container" ref="displayContainer">
+  </div>
+</template>
+
+<style scoped>
+.display-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
